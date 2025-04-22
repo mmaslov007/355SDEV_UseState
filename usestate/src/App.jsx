612da585df8c362c73ecc3bd58f93cdadf293b11
mmaslov007 <maslov.maxwell@student.greenriver.edu>
2025-04-22 11:49:54 -0700

    cleaned up code and added comments

--- usestate/src/App.jsx
+++ usestate/src/App.jsx
@@ -1,14 +1,14 @@
 import RestaurantsContainer from "./components/RestaurantsContainer";
 import { restaurants } from "./data/data.js";
 import "./App.css";
-import { useState } from 'react'; // imported useState
+import { useState } from "react"; // imported useState
 
 // first element: current state
 // second element: function used to update the current state
 // ...restaurants is being passed to the function as a new array using the spread operator
-const [restaurantState, setRestaurants] = useState([...restaurants]);
-
 function App() {
+  const [restaurantState, setRestaurants] = useState([...restaurants]);
+  
   return (
     <div className="App">
       <RestaurantsContainer restaurants={restaurantState}/>
